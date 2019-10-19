afaa5a5c2a4696a452d0e2d3535e134f3453a1ed
Dan Collins <dcollins@linuxmail.org>
2019-10-18 23:50:38 -0500

    Removed email

--- regna/settings.py
+++ regna/settings.py
@@ -91,14 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dcollins31@gmail.com'
-EMAIL_HOST_PASSWORD = 'froglegs2'
-EMAIL_USE_TLS = True
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
