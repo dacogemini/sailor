47892d21f92b316dede248a08c1863c9e3f3caa3
Dan Collins <dcollins@linuxmail.org>
2019-10-21 13:43:24 -0500

    changed static directory

--- regna/settings.py
+++ regna/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$iy-+k(c0zeuo+91uxpjkp&)u3wvfs71!0=&qvyi$_vl*a#fyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
