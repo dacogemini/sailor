ef89677f7c1bebf0e07112d55821aac2d9a71600
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2019-11-14 10:56:02 -0600

    Added sendgrid

--- sendemail/views.py
+++ sendemail/views.py
@@ -1,8 +1,25 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+
+message = Mail(
+    from_email='from_email@example.com',
+    to_emails='to@example.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
 
 
 def emailView(request):
