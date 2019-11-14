7db2d82afc87f1b1fcab9b5dc7a2853b81b6a536
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2019-11-14 11:48:02 -0600

    Update views.py

--- sendemail/views.py
+++ sendemail/views.py
@@ -5,23 +5,23 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 def emailView(request):
-    if request.method == 'GET':
-      form = ContactForm()
+   if request.method == 'GET':
+        form = ContactForm()
     else:
-    form = ContactForm(request.POST)
-       # runs validation routines for all its fields
-       if form.is_valid():
-           first_name = form.cleaned_data['first_name']
-           last_name = form.cleaned_data['last_name']
-           subject = form.cleaned_data['subject']
-           from_email = form.cleaned_data['from_email']
-           message = form.cleaned_data['message']
-           try:
-               send_mail(subject, message, from_email, ['dcollins@linuxmail.org'])
-           except BadHeaderError:
-               return HttpResponse('Invalid header found.')
-           return redirect('success')
-   return render(request, "email.html", {'form': form})
+        form = ContactForm(request.POST)
+        # runs validation routines for all its fields
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['dcollins@linuxmail.org'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "email.html", {'form': form})
 
 
 def successView(request):
